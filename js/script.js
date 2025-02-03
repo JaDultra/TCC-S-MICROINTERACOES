4b67cda58695b42afe38eb035130403338e6a352
Jackson Dultra <70244198+JaDultra@users.noreply.github.com>
2025-02-03 18:27:39 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,279 @@
+// Função para rastrear cliques em categorias (Exemplo: Masculino)
+function trackCategoryClick(category) {
+    gtag('event', 'click', {
+        'event_category': 'Categoria',
+        'event_label': category
+    });
+}
+
+var atualColor = "red"
+
+function Show(file) {
+    var ShowPhoto = document.getElementById('img-grande');
+    newPhoto = "assets/" + atualColor + "-" + file + ".jpg"
+    ShowPhoto.src = newPhoto
+}
+
+function Shoes(color) {
+    var tenis = 1
+    var mudaCor = color
+    while (tenis <= 8) {
+        var thumbs = `/assets/thumbs/${mudaCor}-${tenis}.jpg`
+        var novaCor = document.getElementById(tenis)
+        novaCor.src = thumbs
+        tenis++
+    }
+    atualColor = mudaCor
+    Show('1')
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const favoriteBtn = document.getElementById('favorite-btn');
+    const heartIcon = document.getElementById('heart-icon');
+
+    // Adiciona comportamento ao botão de favoritos
+    if (favoriteBtn && heartIcon) {
+        favoriteBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            if (heartIcon.classList.contains('fa-regular')) {
+                heartIcon.classList.remove('fa-regular');
+                heartIcon.classList.add('fa-solid');
+                heartIcon.style.color = '#ff0000';
+            } else {
+                heartIcon.classList.remove('fa-solid');
+                heartIcon.classList.add('fa-regular');
+                heartIcon.style.color = '';
+            }
+
+            heartIcon.classList.add('animate');
+            setTimeout(() => {
+                heartIcon.classList.remove('animate');
+            }, 600);
+        });
+    }
+
+    // Adiciona comportamento ao botão do carrinho
+    const cartBtn = document.getElementById('cart-btn');
+    const cartIcon = document.getElementById('cart-icon');
+    const sizeInputs = document.querySelectorAll('input[name="tamanho"]');
+    let selectedSize = null;
+
+    if (cartBtn && cartIcon) {
+        sizeInputs.forEach(input => {
+            input.addEventListener('change', (e) => {
+                selectedSize = e.target.id;
+            });
+        });
+
+/*        cartBtn.addEventListener('mouseover', (e) => {
+            cartIcon.style.color = '#00ff11';
+            cartIcon.style.transform = 'rotate(-30deg)';
+        });
+
+        cartBtn.addEventListener('mouseout', (e) => {
+            cartIcon.style.color = '';
+            cartIcon.style.transform = 'rotate(0deg)';
+        });
+*/
+
+        cartBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            if (!selectedSize) {
+                showCustomAlert("Por favor, selecione um tamanho antes de adicionar ao carrinho.");
+                return;
+            }
+
+            const selectedColor = atualColor;
+            localStorage.setItem('selectedColor', selectedColor);
+            localStorage.setItem('selectedSize', selectedSize);
+
+            window.location.href = 'checkout.html';
+        });
+    }
+
+    // Fecha o modal quando o botão fechar for clicado
+    const closeModalBtn = document.getElementById("close-modal");
+    if (closeModalBtn) {
+        closeModalBtn.onclick = function() {
+            document.getElementById("modal").style.display = "none";
+        };
+    }
+
+    // Fecha o alerta customizado ao clicar no botão
+    const alertCloseBtn = document.getElementById('alert-close-btn');
+    if (alertCloseBtn) {
+        alertCloseBtn.addEventListener('click', () => {
+            document.getElementById('custom-alert').style.display = 'none';
+        });
+    }
+
+    // Verifica se está na página de checkout e executa apenas se for necessário
+    if (window.location.pathname.includes('checkout.html')) {
+        handleCheckoutPage();
+    }
+});
+
+
+// Função para manipular a página de checkout
+function handleCheckoutPage() {
+    const selectedColor = localStorage.getItem('selectedColor');
+    const selectedSize = localStorage.getItem('selectedSize');
+
+    if (selectedColor && selectedSize) {
+        const productImage = document.getElementById('produto-carrinho-img');
+        productImage.src = `assets/${selectedColor}-1.jpg`;
+
+        const productSize = document.getElementById('produto-tamanho');
+        productSize.textContent = `Tamanho: ${selectedSize}`;
+    } else {
+        // Apenas exibe um carrinho vazio sem bloquear a navegação
+        const checkoutElement = document.querySelector('.checkout');
+        if (checkoutElement) {
+            checkoutElement.innerHTML = `
+                <h2>Seu carrinho está vazio</h2>
+                <p>Adicione produtos ao carrinho para visualizar nesta página.</p>
+                <a href="index.html" class="btn-voltar">Voltar às Compras</a>
+            `;
+        }
+    }
+}
+
+// Função para exibir o alerta customizado
+function showCustomAlert(message) {
+    const customAlert = document.getElementById('custom-alert');
+    if (customAlert) {
+        const alertMessage = document.querySelector('.custom-alert-content p');
+        alertMessage.textContent = message;
+        customAlert.style.display = 'flex';
+    }
+}
+
+
+// Função para mostrar confetes
+function showConfetti() {
+    confetti({
+        particleCount: 0,
+        spread: 0,
+        origin: { y: 0 }
+    });
+}
+
+
+// Função para finalizar a compra
+function finalizarCompra() {
+    showConfetti();
+    const modal = document.getElementById('modal');
+    if (modal) {
+        modal.style.display = 'flex';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Tamanhos
+    const sizeInputs = document.querySelectorAll('input[name="tamanho"]');
+    sizeInputs.forEach(input => {
+        input.addEventListener('click', () => {
+            gtag('event', 'seleciona-tamanho', {
+                'event_category': 'interaction',
+                'event_label': 'tamanhos',
+                'value': input.id
+            });
+        });
+    });
+
+    // Botão de adicionar ao carrinho
+    const cartBtn = document.getElementById('cart-btn');
+    if (cartBtn) {
+        cartBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            gtag('event', 'adicionar__carrinho', {
+                'event_category': 'interaction',
+                'event_label': 'add-carrinho'
+            });
+        });
+    }
+
+    // Botão de salvar nos favoritos
+    const favoriteBtn = document.getElementById('favorite-btn');
+    if (favoriteBtn) {
+        favoriteBtn.addEventListener('click', () => {
+            gtag('event', 'salvar_favorito', {
+                'event_category': 'interaction',
+                'event_label': 'salvar-fav'
+            });
+        });
+    }
+
+    const masculine = document.querySelector('.masculine');
+    if (masculine) {
+        masculine.addEventListener('click', () => {
+            gtag('event', 'aba-masculino', {
+                'event_category': 'interaction',
+                'event_label': 'masculine'
+            });
+        });
+    }
+
+
+    
+    // Link do WhatsApp
+    const whatsappLink = document.querySelector('.whatsapp-link');
+    if (whatsappLink) {
+        whatsappLink.addEventListener('click', () => {
+            gtag('event', 'link-whatsapp', {
+                'event_category': 'interaction',
+                'event_label': 'whatsapp-link'
+            });
+        });
+    }
+
+    // Benefícios
+    const beneficiosCheckbox = document.getElementById('beneficios');
+    if (beneficiosCheckbox) {
+        beneficiosCheckbox.addEventListener('change', () => {
+            gtag('event', 'beneficios', {
+                'event_category': 'interaction',
+                'event_label': 'beneficios',
+                'value': beneficiosCheckbox.checked ? 'expanded' : 'collapsed'
+            });
+        });
+    }
+
+    // Botão de finalizar compra
+    const finalizarBtn = document.querySelector('.btn-finalizar');
+    if (finalizarBtn) {
+        finalizarBtn.addEventListener('click', () => {
+            gtag('event', 'finalizar-compra', {
+                'event_category': 'interaction',
+                'event_label': 'btn-finalizar'
+            });
+        });
+    }
+        document.querySelector('.btn-fechar').addEventListener('click', function() {
+        window.close(); // Tenta fechar a aba
+    });
+});
+
+function selecionarCor(cor) {
+    localStorage.setItem('corSelecionada', cor); // Salva a cor no localStorage
+    window.location.href = 'index.html'; // Redireciona para a página inicial
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const corSelecionada = localStorage.getItem('corSelecionada');
+    if (corSelecionada) {
+        // Atualizar a imagem principal
+        const imgGrande = document.getElementById('img-grande');
+        if (imgGrande) {
+            imgGrande.src = `assets/${corSelecionada}-1.jpg`;
+        }
+
+        // Atualizar as miniaturas
+        const miniaturas = document.querySelectorAll('.img-menor'); // Seleciona as miniaturas
+        miniaturas.forEach((miniatura, index) => {
+            miniatura.src = `assets/thumbs/${corSelecionada}-${index + 1}.jpg`; // Ajusta os nomes conforme padrão
+        });
+    }
+});
